1f3d2666a9247b950acb1340c69b0299dc74338c
Adowalma <adowalma.adao@selenium.ao>
2026-05-25 22:53:21 +0100

    feat: added toogle to dark and light mode

--- src/app/components/layout/sidebar/sidebar.ts
+++ src/app/components/layout/sidebar/sidebar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DOCUMENT, Inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -7,4 +7,21 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  isDark = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    const htmlTag = this.document.documentElement; // Seleciona a tag <html>
+
+    if (this.isDark) {
+      htmlTag.classList.remove('light');
+      htmlTag.classList.add('dark');
+    } else {
+      htmlTag.classList.remove('dark');
+      htmlTag.classList.add('light');
+    }
+  }
+}
