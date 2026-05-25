76fc59dff4b4eebd8bcc476054f442dd13cdfafe
Adowalma <adowalma.adao@selenium.ao>
2026-05-25 17:29:50 +0100

    style: created components, added regional and dictionary page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Countries } from './pages/countries/countries';
+import { Regional } from './pages/regional/regional';
+import { Timeline } from './pages/timeline/timeline';
+import { Dictionary } from './pages/dictionary/dictionary';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Countries },
+  { path: 'regional', component: Regional },
+  { path: 'temporal', component: Timeline },
+  { path: 'dicionario', component: Dictionary },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './components/layout/sidebar/sidebar';
+import { Navbar } from './components/layout/navbar/navbar';
+import { Footer } from './components/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, Navbar, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('african-pib');
