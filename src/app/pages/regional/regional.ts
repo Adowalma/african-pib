0f4f0bba5f21aec9bc5eb4e055e11bca6585cfe4
Adowalma <adowalma.adao@selenium.ao>
2026-05-27 17:11:51 +0100

    feat: listing regions from json file

--- src/app/pages/regional/regional.ts
+++ src/app/pages/regional/regional.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { Regioes } from '../../services/regionals.json';
 
 @Component({
   selector: 'app-regional',
@@ -8,4 +9,34 @@ import { Component, signal } from '@angular/core';
 })
 export class Regional {
   regional = signal<string>('Continental');
+
+  reginonalData = signal(Regioes);
+
+  regionalList = Regioes;
+
+  years = ['2000', '2006', '2010', '2014', '2019', '2022', '2023'];
+
+  selectedYear = signal<string>('2000');
+
+  filteredData = computed(() => {
+    const regionalList = this.reginonalData();
+    const regionalName = this.regional();
+    const year = this.selectedYear();
+
+    const foundedRegional = regionalList.find((item) => item.Regiao === regionalName);
+    if (!foundedRegional) return null;
+
+    const regionalDataSelected = foundedRegional as any;
+
+    return {
+      pibTotal: regionalDataSelected[`PIB_Total_${year}_Bi`],
+    };
+
+    // console.log(regionalItem['Regiao'][this.regional()]);
+  });
+
+  onYearChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedYear.set(selectElement.value);
+  }
 }
